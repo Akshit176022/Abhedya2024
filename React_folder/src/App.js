8706da49a8bd62f27132b9f31bc8bfcfb1033acb
Naman Srivastava <namansrivastava2004.khs@gmail.com>
2024-01-26 18:05:58 +0530

    images

--- React_folder/src/App.js
+++ React_folder/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-// import Navbar from "./components/Navbar/Navbar";
+//  import Navbar from "./components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from "./components/Home_page/Home";
 // import Leaderboard from "./components/Leaderboard/Leaderboard";
 // import Rules from "./components/Rules/Rules";
-// import Forgot from "./components/Forgot/Forgot";
-import Login from "./components/Login/Login";
+//  import Forgot from "./components/Forgot/Forgot";
+// import Login from "./components/Login/Login";
 
-// import Sign_up from "./components/Sign_up/Signup";
+//  import Sign_up from "./components/Sign_up/Signup";
+ import Navbar_new from "./Navbar_new/Navbar";
 
 
 
@@ -26,13 +27,8 @@ function App(){
 <BrowserRouter>
     <Routes>
 
-      {/* <Route path="/" element={<><Home/><Navbar/></>}/>
-      <Route path="/Leaderboard" element={<><Navbar/> <Leaderboard/></> }/>
-       <Route path="/Rules" element={<><Navbar/> <Rules/></> }/> 
-       <Route path="/Forgot" element={<> <Navbar/><Forgot/></> }/>
-       <Route path="/Login" element={<> <Navbar/><Login/></> }/> */}
-       {/* <Route path="/" element={<><Sign_up/></> }/> */}
-       <Route path="/Login" element={<><Login/></> }/>
+      <Route path="/Navbar" element={<><Navbar_new/></>}/>
+
 
 
 
