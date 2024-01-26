b368cc4cae7d8747de5b7813378e6c9e98017b2a
Naman Srivastava <namansrivastava2004.khs@gmail.com>
2024-01-26 22:39:37 +0530

    final

--- React_folder/src/components/Home_page_new/Home.jsx
+++ React_folder/src/components/Home_page_new/Home.jsx
@@ -1,41 +1,37 @@
 import React from "react";
+import "./style.css";
+import "./style2.css";
 
 
 
 function Home(){
     return(
-    <>
-        <article>
-
-      
-
-<section class="section hero" id="home" aria-label="home"> 
- <div class="container">
-
-    <div class="hero-content">
-
-      <p class="hero-subtitle">Abhedya 3.0</p>
-
-      <h1 class="h1 hero-title">
-        Online <span class="span">Cryptic</span> Hunt
-      </h1>
-
-      <p class="hero-text">
-        Embark on a cosmic adventure with Abhedya, an interstellar cryptic hunt challenging your logical, technical, and creative thinking. 
-      </p>
-
-      <button class="btn skewBg">{/*<a  href="#readmore">*/}Read More</button>
-
-    </div>
-
-    <figure class="hero-banner img-holder" style={{width: 700, height: 700}}>
-       <img src="../../../../React_folder/public/hero2.png" width="700" height="700" alt="hero banner" class="w-100"/> 
-    </figure>
-
-  </div>
-</section>
-</article>
-
+    <><body id="top">
+    
+    <section class="section hero" id="home" aria-label="home" >
+         <div class="container">
+  
+        <div class="hero-content">
+  
+          <p class="hero-subtitle">Abhedya</p>
+  
+          <h1 class="h1 hero-title">
+            Online <span class="span">Cryptic</span> Hunt
+          </h1>
+  
+          <p class="hero-text">
+            Embark on a cosmic adventure with Abhedya, an interstellar cryptic hunt challenging your logical, technical, and creative thinking. 
+          </p>
+  
+          <button class="btn skewBg">Read More</button>
+  
+        </div>
+  
+    
+  
+      </div>
+    </section>
+  </body>
     </>
 
 
