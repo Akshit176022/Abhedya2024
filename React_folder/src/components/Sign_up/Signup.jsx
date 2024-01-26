e1ecdcaac552da9349721123610f9d1a35142a54
akshit Pathania <akshitpathania75@gmail.com>
2024-01-27 02:40:25 +0530

    Navbar fixed and rest pages are added

--- React_folder/src/components/Sign_up/Signup.jsx
+++ React_folder/src/components/Sign_up/Signup.jsx
@@ -10,34 +10,34 @@ function Sign_up(){
     return(
         <>
 
-<div class="card">
-      <div class="main">
-        <div class="header">
-            <h1>Sign Up</h1>
+<div class="cardsignup">
+      <div class="mainsignup">
+        <div class="headersignup">
+         <h1> Sign Up</h1>
         </div>
 
         <form action="./form.html">
-            <div class="components">
+            <div class="componentssignup">
                 <label for="email ">Enter email</label>
                 <input type="text" id="email" name="email" placeholder="name@example.com"/>
             </div>
 
-            <div class="components">
+            <div class="componentssignup">
                 <label for="password ">Enter password</label>
                 <input type="text" id="password" name="password" placeholder="password"/>
             </div>
 
-            <div class="components">
+            <div class="componentssignup">
                 <label for="name  ">Enter username</label>
                 <input type="text" id="name " name="name " placeholder="Your Name"/>
             </div>
 
-            <div class="components">
+            <div class="componentssignup">
                 <label for="name  ">Enter your Phone number</label>
                 <input type="number" id="phone" name="name " placeholder="Phone Number"/>
             </div>
-            <div class="button">
-                <button id="Button">
+            <div class="buttonsignup">
+                <button id="Buttonsignup">
                     SIGN UP
                 </button>
             </div>
--- React_folder/src/components/timeline/timeline.jsx
+++ React_folder/src/components/timeline/timeline.jsx
@@ -6,31 +6,31 @@ function timeline(){
         <>
    <div class="timeline">
         <div class="outer">
-          <div class="card">
+          <div class="cardtimeline">
             <div class="info">
               <h3 class="title">Title 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             </div>
           </div>
-          <div class="card">
+          <div class="cardtimeline">
             <div class="info">
               <h3 class="title">Title 2</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             </div>
           </div>
-          <div class="card">
+          <div class="cardtimeline">
             <div class="info">
               <h3 class="title">Title 3</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             </div>
           </div>
-          <div class="card">
+          <div class="cardtimeline">
             <div class="info">
               <h3 class="title">Title 4</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
             </div>
           </div>
-          <div class="card">
+          <div class="cardtimeline">
             <div class="info">
               <h3 class="title">Title 5</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
@@ -41,4 +41,4 @@ function timeline(){
 </>
     );
 }
-export default timeline;
+export default timeline
