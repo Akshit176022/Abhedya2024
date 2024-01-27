bc83c6b400d079a918cbc7295fc985039710a0c4
akshit Pathania <akshitpathania75@gmail.com>
2024-01-27 13:34:06 +0530

    issues fixed

--- React_folder/src/App.js
+++ React_folder/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login/Login";
-// import Signup from "./components/Sign_up/Signup";
+import Signup from "./components/Sign_up/Signup";
 // import Newhome from "./components/New_home/New_home";
-// import Leaderboard from "./components/Leaderboard/Leaderboard";
-//  import Rules from "./components/Rules/Rules";
-// import Forgot from "./components/Forgot/Forgot";
+import Leaderboard from "./components/Leaderboard/Leaderboard";
+ import Rules from "./components/Rules/Rules";
+import Forgot from "./components/Forgot/Forgot";
 
 // import Home from "./components/Home_page/Home"
-// import Timelines from "./components/timeline/timeline";
+import Timelines from "./components/timeline/timeline";
 import Navbar_new from "./components/Navbar_new/Navbar_new";
-// import Home from "./components/Home_page/Home";
+
 
 
 
@@ -36,15 +36,15 @@ function App(){
     <Routes>
 
 
-       {/* <Route path="/Login" element={<> <Login/></> }/>   */}
-      {/* <Route path="/Signup" element={<> <Signup/></> }/> */}
+       <Route path="/Login" element={<> <Login/></> }/>  
+      <Route path="/Signup" element={<> <Signup/></> }/>
       {/* <Route path="/New_home" element={<> <Newhome/></> }/>   */}
-      {/* <Route path="/Leaderboard" element={<> <Leaderboard/></> }/>  */}
-       {/* <Route path="/Rules" element={<><Rules/> </>}/>  */}
-       {/* <Route path="/Forgot" element={<><Forgot/> </>}/>  */}
-       {/* <Route path="/" element={<><Timelines/> </>}/>   */}
+      <Route path="/Leaderboard" element={<> <Leaderboard/></> }/> 
+       <Route path="/Rules" element={<><Rules/> </>}/> 
+       <Route path="/Forgot" element={<><Forgot/> </>}/> 
+       <Route path="/" element={<><Timelines/> </>}/>  
         {/* <Route path="/" element={<><Home/> </>}/>    */}
-        <Route path="/Nav" element={<> <Navbar_new/><Login/></> }/> 
+        <Route path="/Nav" element={<> <Navbar_new/></> }/> 
     
 
 
